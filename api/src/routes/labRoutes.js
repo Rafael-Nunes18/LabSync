740937ca89a027cd770fa82f92c514b9b3fa89e4
Paulo <phsb0607@gmail.com>
2026-05-29 16:22:52 -0300

    removed duplicate route file

--- api/src/routes/labRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-import { viewFull, delById, insertData, viewId } from '../controllers/cnpqControllers'
-
-router.get('/', viewFull)
-router.post('/', insertData)
-router.get('/:id', viewId)
-router.delete('/:id', delById)
-
-export default router
