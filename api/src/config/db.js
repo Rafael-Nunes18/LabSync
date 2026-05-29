5634a56d3577b7b5bb627e5c29bc9cf5740adf9c
Paulo <phsb0607@gmail.com>
2026-05-29 16:01:28 -0300

    changed name scheme to match project and removed test page

--- api/src/config/db.js
+++ api/src/config/db.js
@@ -1,9 +1,2 @@
-const pg = require('pg')
-const { Pool } = pg
-const pool = new Pool({
-  user: process.env.PGUSER,
-  password: process.env.PGPASS,
-  host: process.env.PGHOST,
-  database: process.env.PGDB
-})
-module.exports = pool
+const sp = require("@supabase/supabase-js")
+sp.createClient()
