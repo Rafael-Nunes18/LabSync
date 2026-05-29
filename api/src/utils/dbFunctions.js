60fc754206f01eccb2183e3af8cc6d089e799d56
Paulo <phsb0607@gmail.com>
2026-05-29 16:09:47 -0300

    renamed some parts and changed most files to ES modules

--- api/src/utils/dbFunctions.js
+++ api/src/utils/dbFunctions.js
@@ -1,25 +1,25 @@
-const pool = require('../config/db')
-exports.viewTable = async (table, param, value) => {
+import { query as _query } from '../config/db'
+export async function viewTable(table, param, value) {
   const tab = table.toString()
   const query = param === undefined ? `SELECT * FROM ${tab}` : `SELECT * FROM ${tab} WHERE ${param.toString()} = ${value.toString()}`
-  const data = await pool.query(query)
+  const data = await _query(query)
   return data
 }
 
-exports.insertCnpq = async (areas, subareas) => {
+export async function insertCnpq(areas, subareas) {
   const sub = subareas.map((item) => item.toString())
   const query = {
     text: 'INSERT INTO cnpq(nome, subareas) VALUES($1, $2)',
     values: [areas.toString(), sub]
   }
-  const res = await pool.query(query)
+  const res = await _query(query)
   return res
 }
-exports.deleteFrom = async (table, value) => {
+export async function deleteFrom(table, value) {
   const query = {
     text: `DELETE FROM ${table.toString()} WHERE id = $1`,
     values: [Number(value)]
   }
-  let del = await pool.query(query)
+  let del = await _query(query)
   return del
 }
