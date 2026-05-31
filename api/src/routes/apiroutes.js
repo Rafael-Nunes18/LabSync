606bd484b88d6c02dd087209e22fc5344302fc83
Fhelipe Mantovan <fhelipe.mantovan007@gmail.com>
2026-05-31 01:49:35 -0300

    Created endpoints for CRUD to the tables
    - Added routes and the actions for Creating, Reading, Updating and Deleting professors, labs and their schedules.

--- api/src/routes/apiroutes.js
+++ api/src/routes/apiroutes.js
@@ -1,5 +1,7 @@
 import { Router } from 'express'
-import { labDeleteById, labInsertData, labViewFull, labViewId } from '../controllers/labController.js'
+import { labDeleteById, labInsertData, labViewFull, labViewId, labUpdateId } from '../controllers/labController.js'
+import { profDeleteById, profInsertData, profViewFull, profViewId, profUpdateId } from '../controllers/labController.js'
+import { resDeleteById, resInsertData, resViewFull, resViewId, resUpdateId } from '../controllers/labController.js'
 
 const apiRouter = Router()
 
@@ -7,5 +9,19 @@ apiRouter.get('/lab', labViewFull)
 apiRouter.post('/lab', labInsertData)
 apiRouter.get('/lab/:id', labViewId)
 apiRouter.delete('/lab/:id', labDeleteById)
+apiRouter.put('/lab/:id', labUpdateId)
+
+apiRouter.get('/prof', profViewFull)
+apiRouter.post('/prof', profInsertData)
+apiRouter.get('/prof/:id', profViewId)
+apiRouter.delete('/prof/:id', profDeleteById)
+apiRouter.put('/prof/:id', profUpdateId)
+
+apiRouter.get('/res', resViewFull)
+apiRouter.post('/res', resInsertData)
+apiRouter.get('/res/:id', resViewId)
+apiRouter.delete('/res/:id', resDeleteById)
+apiRouter.put('/res/:id', resUpdateId)
+
 
 export default apiRouter
